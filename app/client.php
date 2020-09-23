ecd3eb15cce50ffcf96fc2919f0778c4be2db926
moon007 <575865770@qq.com>
2020-09-23 11:16:18 +0800

    tcp

--- app/client.php
+++ app/client.php
@@ -7,13 +7,13 @@
  */
 
 //使用 stream_socket_client 打开 tcp 连接
-$fp = stream_socket_client("tcp://127.0.0.1:2347");
+$fp  = stream_socket_client("tcp://127.0.0.1:2347");
 $msg = "fsockopen send message";
 //向句柄中写入数据
-fwrite($fp,$msg);
+fwrite($fp, $msg);
 $ret = "";
 //循环遍历获取句柄中的数据，其中 feof() 判断文件指针是否指到文件末尾
-while (!feof($fp)){
+while (!feof($fp)) {
     stream_set_timeout($fp, 2);
     $ret .= fgets($fp, 128);
 }
